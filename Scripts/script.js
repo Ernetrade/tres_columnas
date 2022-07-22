8c4d3e2254f5e2096bb2c4d7d900fa7525efd4f6
legiónDev <ernetrades@gmail.com>
2022-07-22 09:51:06 +0200

    Tres columnas sin responsive

---
+++ Scripts/script.js
@@ -0,0 +1,8 @@
+//Guardar el estado (Columna activa)
+let ColumnaActiva = 1
+
+//Seleccionar las columnas
+const columnas = document.querySelectorAll('.columna')
+
+//Escuchar clicks en cada una de ellas
+columnas.forEach(columna, indice)
